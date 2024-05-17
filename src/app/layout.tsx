72d827a74f8f47adc01ae67185ccf6eae418c91e
Jason Rai <jason@withterminal.com>
2024-05-17 16:36:26 -0400

    try to replicate

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ErrorBoundary } from "react-error-boundary";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <ErrorBoundary fallback={<div>Something went wrong</div>}>
+        <body className={inter.className}>{children}</body>
+      </ErrorBoundary>
     </html>
   );
 }
